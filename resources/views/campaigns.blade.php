f3eeb3d32da2926acf208d58af3ebbb7ecfc3e4f
Tayrin Tunzina <ttunzina202145@bscse.uiu.ac.bd>
2023-10-26 11:03:56 +0600

    admin css and js changes

---
+++ resources/views/campaigns.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=campaign, initial-scale=1.0">
+    <title>Campaigns</title>
+</head>
+<body>    
+
+                <div id="content-container">
+					<div id="dashboard-content" class="content-section">
+						<h2>Dashboard Content</h2>
+						<p>This is the dashboard content.</p>
+					</div>
+
+					<div id="campaigns-content" class="content-section">
+						<h2>Campaigns Content</h2>
+						<p>This is the Campaigns content.</p>
+					</div>
+					<div id="donations-content" class="content-section">
+						<h2>Bootstrap Content</h2>
+						<p>This is the Bootstrap content.</p>
+					</div>
+					<div id="donors-content" class="content-section">
+						<h2>Plugins Content</h2>
+						<p>This is the Plugins content.</p>
+					</div>
+
+				</div>
+
+
+</body>
+</html>
