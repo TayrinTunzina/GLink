f93b2eab94faba8fcf94c4a597312fc21fa52582
Rowky <rowky4111@gmail.com>
2023-11-13 16:12:54 +0600

    campaigns

---
+++ database/migrations/2023_11_13_100617_create_campaigns_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->date('deadline');
+            $table->decimal('amount', 10, 2);
+            $table->text('terms');
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaigns');
+    }
+}
